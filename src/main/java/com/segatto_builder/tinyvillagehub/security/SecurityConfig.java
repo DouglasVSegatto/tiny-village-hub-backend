7b4a40f91283187c45aee475a464efaf8a53049e
Segatto <78928583+DouglasVSegatto@users.noreply.github.com>
2025-11-19 20:51:50 -0700

    updated cors for test

--- src/main/java/com/segatto_builder/tinyvillagehub/security/SecurityConfig.java
+++ src/main/java/com/segatto_builder/tinyvillagehub/security/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -73,8 +74,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // ⚠️ current frontend port (e.g., 5173)
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+
+        configuration.setAllowedOrigins(List.of("*"));
+//        configuration.setAllowedOrigins(Arrays.asList(
+//                "http://localhost:5173", // Local dev (still useful)
+//                "http://localhost:3000", // Local dev (still useful)
+//                "https://your-app.com"   // ✅ PRODUCTION DOMAIN
+//        ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
