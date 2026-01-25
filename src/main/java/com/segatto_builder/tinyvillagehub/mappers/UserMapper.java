eb30e0e45a189162a7a2aca235d97bde21d6222e
Segatto <78928583+DouglasVSegatto@users.noreply.github.com>
2026-01-24 19:48:50 -0700

    udpated Mapper to ensure address is pased on UserResponseDto

--- src/main/java/com/segatto_builder/tinyvillagehub/mappers/UserMapper.java
+++ src/main/java/com/segatto_builder/tinyvillagehub/mappers/UserMapper.java
@@ -5,6 +5,7 @@ import com.segatto_builder.tinyvillagehub.dto.user.UserRegistrationDto;
 import com.segatto_builder.tinyvillagehub.dto.user.UserResponseDto;
 import com.segatto_builder.tinyvillagehub.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -12,5 +13,9 @@ public interface UserMapper {
 
     User toModel(UserRegistrationDto dto);
 
-    UserResponseDto toResponse(User item);
+    @Mapping(source = "address.neighborhood", target = "neighborhood")
+    @Mapping(source = "address.city", target = "city")
+    @Mapping(source = "address.state", target = "state")
+    @Mapping(source = "address.country", target = "country")
+    UserResponseDto toResponse(User user);
 }
